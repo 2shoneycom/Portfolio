3277e9bdc0258cfe1004586af22220bec5b50007
2shoneycom <2shoneycom@gmail.com>
2025-09-05 10:18:11 +0900

    (main) First Commit
    Start Point

---
+++ my-project/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import '../styles/style.css';
+
+function NavBar() {
+  return (
+    <nav>
+      
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ my-project/src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar';
+import '../styles/style.css';
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default Home;
